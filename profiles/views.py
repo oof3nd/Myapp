873e89f13972cb78a614d35a7a6beb958d0425b6
Ilya Poroshin <murdererxp@mail.ru>
2017-08-09 02:09:38 +0500

    search add

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DeleteView
 # Create your views here.
+from Test.models import Article
+from menus.models import Item
 
 User = get_user_model()
 
@@ -15,3 +17,15 @@ class ProfileDetailView(DeleteView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username,is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        user =  context['user']
+        query = self.request.GET.get('q')
+        items_exists = Item.objects.filter(user=user).exists()
+        qs = Article.objects.filter(owner = user)
+        if query:
+            qs = qs.filter(title__icontains=query)
+        if items_exists and qs.exists():
+            context['title'] = qs
+        return context
